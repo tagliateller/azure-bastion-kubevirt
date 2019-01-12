192c9b92c132dc94031a477a5a0cfcdfe2d4f302
tagliateller <tagliateller@users.noreply.github.com>
2019-01-12 16:55:36 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum install -y kubectl
 
 $ oc login -u system:admin
 $ oc adm policy add-scc-to-user privileged -n kube-system -z kubevirt-privileged
